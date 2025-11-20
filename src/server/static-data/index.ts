bd7db6eb9363e59f7a5ec9a8275421d37574bdef
josuebedoya348 <josuebedoya348@gmail.com>
2025-11-20 10:54:32 -0500

    Add Rest block proyect

--- src/server/static-data/index.ts
+++ src/server/static-data/index.ts
@@ -1,4 +1,3 @@
-
 export const staticData = {
 	header: {
 		btn: 'Book a Free Class'
@@ -28,5 +27,34 @@ export const staticData = {
 
 	about_instructor: {
 		title: 'About the Instructor',
+	},
+	about_us: {
+		description: '¡Hola! We are The Bogotá Spanish School, and we believe that learning Spanish should be an exciting and deeply personal adventure.'
+	},
+	not_found: {
+		title: 'Don’t panic, we won’t need to put it in nice.',
+		btn: 'Back to homepage'
+	},
+	plans_and_pricing: {
+		btn_plan: 'Get in touch'
+	},
+	social: {
+		icons: {
+			'0': 'fa-brands fa-facebook-f',
+			'1': 'fa-brands fa-instagram',
+			'2': 'fa-brands fa-twitter',
+			'3': 'fa-brands fa-tiktok',
+			'4': 'fa-brands fa-youtube',
+			'5': 'fa-brands fa-linkedin-in',
+			'6': 'fa-brands fa-flickr',
+			'7': 'fa-brands fa-pinterest-p',
+			'8': 'fa-brands fa-google',
+			'default': 'fa-brands fa-globe'
+		}
+	},
+	error_pages: {
+		500: {
+			title: '500 - Internal Server Error',
+		}
 	}
 };
